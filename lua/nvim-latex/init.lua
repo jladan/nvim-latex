c2cf79cdf0526c14093b31b6261125b1547ca4f7
John Ladan <john@ladan.ca>
2021-11-11 23:31:58 -0500

    add a smarter method to find the root path and main tex file

--- lua/nvim-latex/init.lua
+++ lua/nvim-latex/init.lua
@@ -8,19 +8,70 @@
 local ts_query = require("nvim-treesitter.query")
 local utils = require("nvim-latex.utils")
 
+local function has_docclass(bufnr)
+    -- Store the current location before searching
+    local pos = vim.fn.getcurpos()
+    local curbuf = vim.fn.bufnr()
+    bufnr = bufnr or curbuf
+
+    -- search for the document class macro
+    vim.api.nvim_set_current_buf(bufnr)
+    local found = vim.fn.search("^\\s*\\\\documentclass")
+    -- return to original position
+    -- This may cause the window to center on current line (:normal zz)
+    vim.api.nvim_set_current_buf(curbuf)
+    vim.fn.cursor(pos[2],pos[3])
+    return found > 0
+end
+
+local function main_from_latexmkrc(rc)
+    local lines = vim.fn.readfile(rc)
+    for _, line in ipairs(lines) do
+        match = line:match( '@default_files.-[\'"](.-)[\'"]')
+        if match then 
+            return match
+        end
+    end
+end
+
 local M = {}
 
--- Set the name of the main latex document that's being edited
--- This will matter for multi-file documents
+M.has_docclass = has_docclass
+M.test = main_from_latexmkrc
+
+---- Set the name of the main latex document that's being edited
+--
+-- This will matter for multi-file documents.
+-- Current strategy choose in order:
+--  - the file if it has a documentclass declaration
+--  - the first default file in the .latexmkrc
+--  - the file as a last resort
+--
 -- TODO should this be a buffer variable or module-level?
-function M.set_document_root(bufnr, rootfile)
+function M.set_document_root(bufnr)
     -- Default to current buffer, and buffer filename
     bufnr = bufnr or vim.fn.bufnr()
-    rootfile = rootfile or vim.fn.bufname(bufnr)
+    local thisfile = vim.fn.bufname(bufnr)
+    thisfile = vim.fn.fnamemodify(thisfile, ":p")
 
-    -- now get the absolute path and filename for use in the rest
-    vim.b.latex_root = vim.fn.fnamemodify(rootfile, ":p:h")
-    vim.b.latex_main = vim.fn.fnamemodify(rootfile, ":p")
+    -- Start with the directory of rootfile
+    local thisdir = vim.fn.fnamemodify(thisfile, ":h")
+    -- If \documentclass is in thisfile, then it should be the root
+    if has_docclass(bufnr) then
+        vim.b.latex_root = thisdir
+        vim.b.latex_main = thisfile
+    else
+        -- Try looking for a latexmkrc
+        local latexmkrc = vim.fn.findfile('.latexmkrc', '.;')
+        if latexmkrc ~= "" then
+            vim.b.latex_root = vim.fn.fnamemodify(latexmkrc, ':p:h')
+            vim.b.latex_main = vim.b.latex_root .. '/' .. main_from_latexmkrc(latexmkrc)
+        else
+            -- If all else fails, just use the current file?
+            vim.b.latex_root = thisdir
+            vim.b.latex_main = thisfile
+        end
+    end
 
     return vim.b.latex_root
 end
