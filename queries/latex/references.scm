522bfebb994531de87b73e12f6eddde1ca3219f1
John Ladan <john@ladan.ca>
2021-11-12 23:20:58 -0500

    Make the label queries more specific to equations, floats, and sections

--- queries/latex/references.scm
+++ queries/latex/references.scm
@@ -1,11 +1,21 @@
 ; Queries used to find metadata in latex documents
 
-; Matches the name in \label{}
-((label_definition
-    name: (word) @label))
-; Matches the cross-reference name in \ref{}
-((label_reference
-    label: (word) @ref))
+; Matching section and float labels {{{
+((_
+    ;; All section macros have a bracegroup, but environments don't
+    text: ((brace_group))
+    (label_definition name: (word) @label)
+ ) 
+)
+
+((environment
+    begin: (begin name: (word) @float-name)
+    (label_definition name: (word) @label)
+ ) @float
+ (#any-of? @float-name "figure" "table")
+)
+
+; }}}
 
 ; Matching equations {{{
 ((equation_label_reference
@@ -15,10 +25,10 @@
 ; This will match each label, but the related captures will match multiple
 ; times for each environment
 ((environment
-    begin: (begin name: (word) @eq-env-name)
+    begin: (begin name: (word) @eq-name)
     (label_definition name: (word) @eq-label)?
  )
- (#any-of? @eq-env-name "align" "align*" "equation" "equation*")
+ (#any-of? @eq-name "align" "align*" "equation" "equation*")
 )
 
 
